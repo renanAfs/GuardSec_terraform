dce6a9de56a5e1246d4db27ebcb744202902b643
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-27 21:57:56 -0300

    versao teste

--- terraform/aws/00-provider.tf
+++ terraform/aws/00-provider.tf
@@ -2,18 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.73.0"
+      version = "~> 5.0"
     }
-  }
-  backend "s3" {
-    bucket         = "guardsec-tf-s3"
-    key            = "tfstate"
-    dynamodb_table = "guardsec-tf-dynamo"
-    region         = "us-east-1"
   }
 }
 
-
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
